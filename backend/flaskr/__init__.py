119c8bd9bacbee79520cc56fe5ee22bb61e80116
Rwego Patrick <rwego.patrick.50@gmail.com>
2022-09-04 07:07:28 -0600

    modify main flask app

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,4 +1,6 @@
 import os
+from sre_constants import SUCCESS
+from unicodedata import category
 from flask import Flask, request, abort, jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -53,20 +55,24 @@ def create_app(test_config=None):
 
     @app.route('/categories')
     def getall_categories():
-        # By Default GET Method request
+        # by Default GET Method request
+
         # get all categories
         categories = Category.query.order_by(Category.id).all()
 
-        # Dictionary to hold retrieved categories
-        cat_dict = {}
-        for cat in categories:
-            cat_dict[cat.id] = cat.type
+        # dictionary to hold retrieved categories
+        categoryDict = {cat.id: cat.type for cat in categories}
 
-        # Abort if no category found
-        if len(categories) == 0:
+        # abort if no category found
+        if len(categoryDict) == 0:
             abort(404)
 
-        return jsonify(success=True, categories=cat_dict, total_categories=len(Category.query.all()))
+        # return success message
+        return jsonify(
+            success=True,
+            categories=categoryDict,
+            total_categories=len(Category.query.all())
+        )
 
     # -------------------------------------------------------------
     # endpoint handle GET requests /all questions + pagination(10)
@@ -74,27 +80,31 @@ def create_app(test_config=None):
 
     @ app.route('/questions')
     def getall_questions():
-        # By Default GET Method request
+        # by Default GET Method request
+
         # get all questions respect to pagination aspect of 10
         selection = Question.query.order_by(Question.id).all()
-        current_questions = paginate_questions(request, selection)
 
-        # Dictionary to hold categories
-        categories = Category.query.order_by(Category.type).all()
-        cat_dict = {}
-        for cat in categories:
-            cat_dict[cat.id] = cat.type
+        # pagination
+        selected_questions = paginate_questions(request, selection)
+
+        # get all categories
+        categories = Category.query.order_by(Category.id).all()
+
+        # dictionary to hold categories
+        categoryDict = {cat.id: cat.type for cat in categories}
 
         # Abort if no questions found
-        if len(current_questions) == 0:
+        if len(selected_questions) == 0:
             abort(404)
 
+        # return success message
         return jsonify(
             success=True,
-            questions=current_questions,
+            questions=selected_questions,
             total_questions=len(selection),
-            current_category={},
-            categories=cat_dict
+            categories=categoryDict,
+            current_category='all available'
         )
 
     # -------------------------------------------------
@@ -103,25 +113,25 @@ def create_app(test_config=None):
 
     @ app.route("/questions/<int:id>", methods=['DELETE'])
     def remove_question(id):
+
         # Get question filter by id
+        question = Question.query.filter_by(id=id).one_or_none()
+
         try:
-            question = Question.query.filter_by(id=id).one_or_none()
-
-            # Abort is no question found by id
-            if question is None:
-                abort(404)
-
-            # Else delete that question and return success message
+            # delete that question
             question.delete()
 
-            return jsonify(
-                success=True,
-                deleted=id,
-                total_qts=len(Question.query.all())
-            )
         except:
-            # Abort for no success delete
-            abort(422)
+            # Abort for no question found
+            if question is None:
+                abort(422)
+
+        # return success message
+        return jsonify(
+            success=True,
+            deleted_question=question.format(),
+            remaining_questions=len(Question.query.all())
+        )
 
     # ---------------------------------------------
     # endpoint handle POST requests /questions
@@ -134,7 +144,7 @@ def create_app(test_config=None):
 
         # Abort no data found
         if not ('question' in body and 'answer' in body and 'difficulty' in body and 'category' in body):
-            abort(405)
+            abort(406)
 
         # Adding ...
         add_question = body.get('question', '')
@@ -142,10 +152,10 @@ def create_app(test_config=None):
         add_difficulty = body.get('difficulty', '')
         add_category = body.get('category', '')
 
-        # Abort for storing empty question
-        if ((add_question == '') or (add_answer == '') or (add_difficulty == '') or (add_category == '')):
-            abort(405)
-        try:
+        # if not storing empty question continue...
+        # raise error alert in frontend
+        if not ((add_question == '') or (add_answer == '') or (add_difficulty == '') or (add_category == '')):
+
             # Inserting ...
             question = Question(
                 question=add_question,
@@ -153,20 +163,16 @@ def create_app(test_config=None):
                 difficulty=add_difficulty,
                 category=add_category
             )
+
             # Storing ...
             question.insert()
 
-            # return a success message
-            return jsonify(
-                success=True,
-                created=question.id,
-                new_question=question.question,
-                tot_questions=len(Question.query.all())
-            )
-
-        except:
-            # Error in creating a question, Abort!
-            abort(405)
+        # return a success message
+        return jsonify(
+            success=True,
+            created_question=question.format(),
+            total_questions=len(Question.query.all())
+        )
 
     # -----------------------------------------------------------------
     # endpoint handle POST requests get questions based on search Term
@@ -176,22 +182,24 @@ def create_app(test_config=None):
     def find_questions():
         # Requesting data
         body = request.get_json()
-        search = body.get('searchTerm', None)
+        search_Term = Question.query.filter(
+            Question.question.ilike('%{}%'.format(body.get('searchTerm')))
+        ).all()
 
-        # filter search with the given search Term
-        if search:
-            search_Term = Question.query.filter(
-                Question.question.ilike(f'%{search}%')).all()
+        if not body.get('searchTerm'):
+            # abort when nothing inserted
+            abort(404)
 
-            # return a success message
-            return jsonify(
-                success=True,
-                questions=[question.format() for question in search_Term],
-                total_questions=len(search_Term),
-                current_category=None
-            )
-        # else Abort!
-        abort(404)
+        # else paginate if exceed above 10
+        searched_questions = paginate_questions(request, search_Term)
+
+        # return success message
+        return jsonify(
+            success=True,
+            questions=searched_questions,
+            total_questions=len(search_Term),
+            current_category='any'
+        )
     # -------------------------------------------------------------
     # endpoint handle GET requests get question based on category
     # -------------------------------------------------------------
@@ -200,57 +208,64 @@ def create_app(test_config=None):
     def get_questions_by_category(cat_id):
         # By Default GET Method request
         # Filter Question with the given Category id
-        try:
-            all_questions = Question.query.filter(
-                Question.category == str(cat_id)
-            ).all()
+        all_questions = Question.query.filter_by(
+            category=cat_id
+        ).all()
 
-            current_question = paginate_questions(request, all_questions)
+        # pagination
+        selected_questions = paginate_questions(request, all_questions)
 
-            if len(current_question) == 0:
-                abort(400)
-            # return a success message
-            return jsonify(
-                success=True,
-                questions=[qts.format() for qts in all_questions],
-                total_questions=len(all_questions),
-                current_category=cat_id
-            )
-        except:
-            # else Abort when not found!
+        # abort when no questions found
+        if len(selected_questions) == 0:
             abort(404)
+
+        # dictionary to hold retrieved questions
+        questionDict = {qts.id: qts.question for qts in all_questions}
+
+        # return a success message
+        return jsonify(
+            success=True,
+            current_category=cat_id,
+            questions=questionDict,
+            total_questions=len(all_questions)
+
+        )
     # ----------------------------------------------------------------------------------
     # endpoint handle POST requests to get questions to play with respect to categories
     # ----------------------------------------------------------------------------------
 
     @ app.route('/quizzes', methods=['POST'])
     def start_quiz():
+        # requesting data
         body = request.get_json()
 
+        # abort when no data found
         if not ('quiz_category' in body and 'previous_questions' in body):
             abort(422)
 
+        # adding into variables
         quiz_cat = body.get('quiz_category')
-        prev_questions = body.get('previous_questions')
+        last_questions = body.get('previous_questions')
 
-        try:
-            if quiz_cat['type'] == 'click':
-                untaken_questions = Question.query.filter(
-                    Question.id.notin_((prev_questions))).all()
-            else:
-                untaken_questions = Question.query.filter_by(
-                    category=quiz_cat['id']).filter(Question.id.notin_((prev_questions))).all()
+        # category not mention filter all question
+        question = Question.query.filter(
+            Question.id.notin_(last_questions)).all() if not quiz_cat['id'] else ''
 
-            question = untaken_questions[
-                random.randrange(0, len(untaken_questions))
-            ].format() if len(untaken_questions) > 0 else None
+        # filter questions by category
+        if quiz_cat['id']:
+            question = Question.query.filter(
+                Question.category == quiz_cat['id'], Question.id.notin_(last_questions)).all()
 
-            return jsonify(
-                success=True,
-                question=question
-            )
-        except:
-            abort(422)
+        # random question
+        new_question = random.choice(
+            question).format() if len(question) > 0 else 'no questions found!'
+
+        return jsonify(
+            success=True,
+            question=new_question,
+            current_category=quiz_cat['id'],
+            remaining_question=len(question)
+        )
 
     # -------------------------------------------------------------
     # Expected error handlers
@@ -295,5 +310,13 @@ def create_app(test_config=None):
             error=500,
             message='Internal server error'
         ), 500
+
+    @ app.errorhandler(406)
+    def notAcceptable_error(error):
+        return jsonify(
+            success=False,
+            error=406,
+            message='Not Acceptable'
+        ), 406
 
     return app
