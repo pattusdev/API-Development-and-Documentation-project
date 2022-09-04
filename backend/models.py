d13f2a55f054450011bbc11132e83e6868dffdd8
Rwego Patrick <rwego.patrick.50@gmail.com>
2022-09-04 06:54:08 -0600

    modify app config,remove jsonify sort method

--- backend/models.py
+++ backend/models.py
@@ -18,6 +18,7 @@ setup_db(app)
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config['JSON_SORT_KEYS'] = False
     db.app = app
     db.init_app(app)
     db.create_all()
