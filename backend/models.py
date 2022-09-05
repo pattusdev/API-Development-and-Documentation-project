d71d51aa03c5af8a8067d94ec4879fcc63469e65
Rwego Patrick <rwego.patrick.50@gmail.com>
2022-09-05 02:44:05 -0600

    dotenv

--- backend/models.py
+++ backend/models.py
@@ -2,10 +2,11 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from settings import DB_NAME, DB_USER, DB_PASSWORD
 
-database_name = 'trivia'
+database_name = DB_NAME
 database_path = "postgresql://{}:{}@{}/{}".format(
-    "DB_USER", "<PASSWORD>", "localhost:5432", database_name)
+    DB_USER, DB_PASSWORD, "localhost:5432", database_name)
 
 db = SQLAlchemy()
 
