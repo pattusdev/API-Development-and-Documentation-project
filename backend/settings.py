ebddc8b4f5517fc46006652a0fde3e1495ad4729
Rwego Patrick <rwego.patrick.50@gmail.com>
2022-09-05 11:18:29 +0200

    add dotenv

--- backend/settings.py
+++ backend/settings.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-DB_NAME=os.environ.get('DB_NAME')
-DB_USER=os.environ.get('DB_USER')
-DB_PASSWORD=os.environ.get('DB_PASSWORD')
+DB_NAME = os.environ.get('DB_NAME')
+TEST_DB_NAME = os.environ.get('TEST_DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from settings import TEST_DB_NAME, DB_USER, DB_PASSWORD
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -14,9 +15,9 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
+        self.database_name = TEST_DB_NAME
         self.database_path = "postgresql://{}:{}@{}/{}".format(
-            "rwego", "admin123", 'localhost:5432', self.database_name)
+            DB_USER, DB_PASSWORD, 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
